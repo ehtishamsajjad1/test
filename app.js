96090a015cc58373124cb1ffbdf649b8fbf07253
Ehtisham Sajjad <47937929+ehtishamsajjad1@users.noreply.github.com>
2021-08-13 22:09:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let firstCard = 3;
+let secondCard = 9;
+let sum = firstCard + secondCard;
+console.log(sum);
+let randNum = 13;
+if (randNum === 12) {
+    console.log('Horray! You won');
+} else {
+    console.log('Bad luck');
+}
